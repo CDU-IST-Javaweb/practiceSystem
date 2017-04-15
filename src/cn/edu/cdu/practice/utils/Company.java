944f8dd32c7d3e2ea0fb208cd0511794d6062349
MrC <974927257@qq.com>
2017-04-15 09:13:21 +0800

    新增加的接口

---
+++ src/cn/edu/cdu/practice/utils/Company.java
@@ -0,0 +1,13 @@
+
+package cn.edu.cdu.practice.utils;
+/**
+ * <p>Title: Company</p>
+ * <p>Description: </p>
+ * <p>Company: www.com.panda</p> 
+ * @author	陈先森
+ * @date	2017年4月15日上午9:08:35
+ * @version 1.0
+ */
+public class Company {
+
+}

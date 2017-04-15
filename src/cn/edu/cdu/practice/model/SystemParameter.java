2c7758ad3c0b16ebdf3f2dc40384f0c0250d8324
dennisyuxi <yuxi@cdu.edu.cn>
2017-04-15 15:27:44 +0800

    于曦删除SystemParameterId.java

--- src/cn/edu/cdu/practice/model/SystemParameter.java
+++ src/cn/edu/cdu/practice/model/SystemParameter.java
@@ -1,5 +1,7 @@
 package cn.edu.cdu.practice.model;
 
+import java.sql.Timestamp;
+
 /**
  * @Copyright (C), 2017, 成都大学信息科学与工程学院JavaWeb教材编写组.
  * @FileName Company.java
@@ -15,27 +17,201 @@ public class SystemParameter implements java.io.Serializable {
 
 	// Fields
 
-	private SystemParameterId id;
+		private String adminUsername;
+		private String adminPassword;
+		private String invitationCode;
+		private Timestamp releaseProjectStartDate;
+		private Timestamp releaseProjectEndDate;
+		private Timestamp studentSelStartDate;
+		private Timestamp studentSelEndDate;
+		private Integer studentSelMaxnum;
 
-	// Constructors
+		// Constructors
 
-	/** default constructor */
-	public SystemParameter() {
-	}
+		/** default constructor */
+		public SystemParameter() {
+		}
 
-	/** full constructor */
-	public SystemParameter(SystemParameterId id) {
-		this.id = id;
-	}
+		/** minimal constructor */
+		public SystemParameter(String adminUsername, String adminPassword,
+				String invitationCode, Integer studentSelMaxnum) {
+			this.adminUsername = adminUsername;
+			this.adminPassword = adminPassword;
+			this.invitationCode = invitationCode;
+			this.studentSelMaxnum = studentSelMaxnum;
+		}
 
-	// Property accessors
+		/** full constructor */
+		public SystemParameter(String adminUsername, String adminPassword,
+				String invitationCode, Timestamp releaseProjectStartDate,
+				Timestamp releaseProjectEndDate, Timestamp studentSelStartDate,
+				Timestamp studentSelEndDate, Integer studentSelMaxnum) {
+			this.adminUsername = adminUsername;
+			this.adminPassword = adminPassword;
+			this.invitationCode = invitationCode;
+			this.releaseProjectStartDate = releaseProjectStartDate;
+			this.releaseProjectEndDate = releaseProjectEndDate;
+			this.studentSelStartDate = studentSelStartDate;
+			this.studentSelEndDate = studentSelEndDate;
+			this.studentSelMaxnum = studentSelMaxnum;
+		}
 
-	public SystemParameterId getId() {
-		return this.id;
-	}
+		// Property accessors
 
-	public void setId(SystemParameterId id) {
-		this.id = id;
-	}
+		public String getAdminUsername() {
+			return this.adminUsername;
+		}
+
+		public void setAdminUsername(String adminUsername) {
+			this.adminUsername = adminUsername;
+		}
+
+		public String getAdminPassword() {
+			return this.adminPassword;
+		}
+
+		public void setAdminPassword(String adminPassword) {
+			this.adminPassword = adminPassword;
+		}
+
+		public String getInvitationCode() {
+			return this.invitationCode;
+		}
+
+		public void setInvitationCode(String invitationCode) {
+			this.invitationCode = invitationCode;
+		}
+
+		public Timestamp getReleaseProjectStartDate() {
+			return this.releaseProjectStartDate;
+		}
+
+		public void setReleaseProjectStartDate(Timestamp releaseProjectStartDate) {
+			this.releaseProjectStartDate = releaseProjectStartDate;
+		}
+
+		public Timestamp getReleaseProjectEndDate() {
+			return this.releaseProjectEndDate;
+		}
+
+		public void setReleaseProjectEndDate(Timestamp releaseProjectEndDate) {
+			this.releaseProjectEndDate = releaseProjectEndDate;
+		}
+
+		public Timestamp getStudentSelStartDate() {
+			return this.studentSelStartDate;
+		}
+
+		public void setStudentSelStartDate(Timestamp studentSelStartDate) {
+			this.studentSelStartDate = studentSelStartDate;
+		}
+
+		public Timestamp getStudentSelEndDate() {
+			return this.studentSelEndDate;
+		}
+
+		public void setStudentSelEndDate(Timestamp studentSelEndDate) {
+			this.studentSelEndDate = studentSelEndDate;
+		}
+
+		public Integer getStudentSelMaxnum() {
+			return this.studentSelMaxnum;
+		}
+
+		public void setStudentSelMaxnum(Integer studentSelMaxnum) {
+			this.studentSelMaxnum = studentSelMaxnum;
+		}
+
+		public boolean equals(Object other) {
+			if ((this == other))
+				return true;
+			if ((other == null))
+				return false;
+			if (!(other instanceof SystemParameter))
+				return false;
+			SystemParameter castOther = (SystemParameter) other;
+
+			return ((this.getAdminUsername() == castOther.getAdminUsername()) || (this
+					.getAdminUsername() != null
+					&& castOther.getAdminUsername() != null && this
+					.getAdminUsername().equals(castOther.getAdminUsername())))
+					&& ((this.getAdminPassword() == castOther.getAdminPassword()) || (this
+							.getAdminPassword() != null
+							&& castOther.getAdminPassword() != null && this
+							.getAdminPassword()
+							.equals(castOther.getAdminPassword())))
+					&& ((this.getInvitationCode() == castOther.getInvitationCode()) || (this
+							.getInvitationCode() != null
+							&& castOther.getInvitationCode() != null && this
+							.getInvitationCode().equals(
+									castOther.getInvitationCode())))
+					&& ((this.getReleaseProjectStartDate() == castOther
+							.getReleaseProjectStartDate()) || (this
+							.getReleaseProjectStartDate() != null
+							&& castOther.getReleaseProjectStartDate() != null && this
+							.getReleaseProjectStartDate().equals(
+									castOther.getReleaseProjectStartDate())))
+					&& ((this.getReleaseProjectEndDate() == castOther
+							.getReleaseProjectEndDate()) || (this
+							.getReleaseProjectEndDate() != null
+							&& castOther.getReleaseProjectEndDate() != null && this
+							.getReleaseProjectEndDate().equals(
+									castOther.getReleaseProjectEndDate())))
+					&& ((this.getStudentSelStartDate() == castOther
+							.getStudentSelStartDate()) || (this
+							.getStudentSelStartDate() != null
+							&& castOther.getStudentSelStartDate() != null && this
+							.getStudentSelStartDate().equals(
+									castOther.getStudentSelStartDate())))
+					&& ((this.getStudentSelEndDate() == castOther
+							.getStudentSelEndDate()) || (this
+							.getStudentSelEndDate() != null
+							&& castOther.getStudentSelEndDate() != null && this
+							.getStudentSelEndDate().equals(
+									castOther.getStudentSelEndDate())))
+					&& ((this.getStudentSelMaxnum() == castOther
+							.getStudentSelMaxnum()) || (this.getStudentSelMaxnum() != null
+							&& castOther.getStudentSelMaxnum() != null && this
+							.getStudentSelMaxnum().equals(
+									castOther.getStudentSelMaxnum())));
+		}
+
+		public int hashCode() {
+			int result = 17;
+
+			result = 37
+					* result
+					+ (getAdminUsername() == null ? 0 : this.getAdminUsername()
+							.hashCode());
+			result = 37
+					* result
+					+ (getAdminPassword() == null ? 0 : this.getAdminPassword()
+							.hashCode());
+			result = 37
+					* result
+					+ (getInvitationCode() == null ? 0 : this.getInvitationCode()
+							.hashCode());
+			result = 37
+					* result
+					+ (getReleaseProjectStartDate() == null ? 0 : this
+							.getReleaseProjectStartDate().hashCode());
+			result = 37
+					* result
+					+ (getReleaseProjectEndDate() == null ? 0 : this
+							.getReleaseProjectEndDate().hashCode());
+			result = 37
+					* result
+					+ (getStudentSelStartDate() == null ? 0 : this
+							.getStudentSelStartDate().hashCode());
+			result = 37
+					* result
+					+ (getStudentSelEndDate() == null ? 0 : this
+							.getStudentSelEndDate().hashCode());
+			result = 37
+					* result
+					+ (getStudentSelMaxnum() == null ? 0 : this
+							.getStudentSelMaxnum().hashCode());
+			return result;
+		}
 
 }

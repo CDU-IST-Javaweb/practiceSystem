e3149f57685f338d71a5c879f4e2b79633ed8295
dennisyuxi <yuxi@cdu.edu.cn>
2017-04-27 22:51:19 +0800

    于曦：实现登录功能和防sql注入验证

--- src/cn/edu/cdu/practice/service/UserService.java
+++ src/cn/edu/cdu/practice/service/UserService.java
@@ -12,9 +12,11 @@ package cn.edu.cdu.practice.service;
 public interface UserService {
 	//用户登录时，在页面选择角色，然后输入需要的参数，如果验证码和session中的一致，则进行下一步验证
 	//如果role=1，进企业表；如果role=2，进学生表；如果role=9，进系统参数表
-	public boolean login(String account, String password, String Verification_Code, String Session_Verification_Code,String role);
+	public boolean login(String account, String password, String Verification_Code, String role,String vchidden);
 	//用户输入密保邮箱后，获得动态验证码
 	public String getPassBack(String mailbox);
 	//用户两次输入的密码要一致，这个验证在前台页面完成
 	public boolean resetPass(String password, String Verification_Code);
+	public boolean register(String rscode, String qyname, String qyusername, String password, String confirmPassword,
+			String email, String verificationCode, String captcha);
 }
